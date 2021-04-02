673b5b7dc32b69ff00d4605ee5b64b396bec9891
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-04-02 17:43:16 +0530

    Update Subset2.java

--- Subset Related Problem/Subset2.java
+++ Subset Related Problem/Subset2.java
@@ -1,24 +1,24 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>>l=new ArrayList<>();
-        List<Integer>l1=new ArrayList<>();
+        List<List<Integer>>ans=new ArrayList<>();
+        List<Integer>curr=new ArrayList<>();
         Arrays.sort(nums);
-        buildSubset1(nums,l,l1,0);
-        return l;
+        buildSubset1(nums,ans,curr,0);
+        return ans;
         
     }
-    public void buildSubset1(int nums[],List<List<Integer>>l,List<Integer>l1,int startIndex)
+    public void buildSubset1(int nums[],List<List<Integer>>ans,List<Integer>curr,int startIndex)
     {
-        l.add(new ArrayList<>(l1));
+        ans.add(new ArrayList<>(curr));
         for(int i=startIndex;i<nums.length;i++)
         {
             if(i!=startIndex&&nums[i]==nums[i-1])
             {
                 continue;
             }
-                l1.add(nums[i]);
-                buildSubset1(nums,l,l1,i+1);
-                l1.remove(l1.size()-1);
+                curr.add(nums[i]);
+                buildSubset1(nums,ans,curr,i+1);
+                curr.remove(curr.size()-1);
             
         }
     }
